66d17c6a5ba6ff4d5ddb7030ee4868a236b87dcb
kulkarni-vivek7 <kulkarnivivek098@gmail.com>
2025-07-31 10:20:59 +0530

    fix: replaced AntPathRequestMatcher which is deprecated with PathPatternRequestMatcher and removed all unused imports

--- src/main/java/com/example/gym_management_system_backend_ai/config/SecurityConfig.java
+++ src/main/java/com/example/gym_management_system_backend_ai/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.example.gym_management_system_backend_ai.config;
 
+//Bug Fix: Removed All Unnecessary Imports
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,18 +36,17 @@ public class SecurityConfig {
 		http.cors(c -> c.configurationSource(corsConfigurationSource()));
 		
 		http
-			.csrf(csrf -> csrf.disable())
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers("/api/auth/login",
 							"/api/auth/register",
 							"/api/auth/send-otp-email").permitAll()
-					.requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
-					.requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui.html")).permitAll()
-					.requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
+//					Bug Fix: Using PathPatternRequestMatcher instead of AntPathRequestMatcher which is deprecated, to match the paths correctly
+					.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/v3/api-docs/**")).permitAll()
+					.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/swagger-ui.html")).permitAll()
+					.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/swagger-ui/**")).permitAll()
 					.anyRequest().authenticated()
 					);
-		
-//		http.httpBasic(Customizer.withDefaults());
 		
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
